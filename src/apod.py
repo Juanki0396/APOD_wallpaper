fd8b1f12d5b7a868cbae9b5f9599b22518470110
Juanki0396 <juanki0396@gmail.com>
2022-06-25 01:43:56 +0200

    Success in retrieving a APOD image

--- src/apod.py
+++ src/apod.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
+from io import BytesIO
 from time import localtime
 
 import requests
 from bs4 import BeautifulSoup
+from PIL import Image
+
+APOD_URL = "https://apod.nasa.gov/apod/"
 
 
 @dataclass
@@ -13,9 +17,14 @@ class DateHandler:
     month: int = localtime().tm_mon
     day: int = localtime().tm_mday
 
+    def __post_init__(self) -> None:
+        """Checks for correct year"""
+        if self.year < 1995 or self.year > localtime().tm_year:
+            raise ValueError(f"APODs  only exist from 1995 to {localtime().tm_year}")
+
     @property
     def apod_date(self) -> str:
-        return f"{self.year - 2000}{self.month:02d}{self.day:02d}"
+        return f"{str(self.year)[-2:]}{self.month:02d}{self.day:02d}"
 
 
 class APODRetrieveError(Exception):
@@ -33,25 +42,34 @@ class Retriever:
     def __init__(self, date: DateHandler) -> None:
 
         self.date = date
-        self.url = f"https://apod.nasa.gov/apod/ap{date.apod_date}.html"
+        self.response = Retriever.get_response(href=f"ap{self.date.apod_date}.html")
+        self.html = Retriever.parse_html(self.response)
 
-    def get_response(self) -> requests.Response:
+    @staticmethod
+    def get_response(href: str) -> requests.Response:
 
-        response = requests.get(self.url)
-
+        url = f"{APOD_URL}{href}"
+        response = requests.get(url)
         if response.status_code != requests.codes.ok:
-            raise APODRetrieveError(response, "The selected APOD is not reachable.")
-
+            raise APODRetrieveError(response, "The site {url} is not reacheble")
         return response
 
+    @staticmethod
+    def parse_html(response: requests.Response) -> BeautifulSoup:
+        return BeautifulSoup(response.text, "html.parser")
 
-class HTMLParser:
-    pass
+    def get_image(self) -> Image.Image:
+        img_tag = self.html.find("img")
+        img_href = img_tag["src"]
+        img_response = requests.get(f"{APOD_URL}{img_href}")
+        img = Image.open(BytesIO(img_response.content))
+        return img
 
 
 def main():
-    date = DateHandler()
-    print(date.apod_date)
+    date = DateHandler(year=2022, month=6, day=24)
+    retriever = Retriever(date)
+    print(retriever.get_image())
 
 
 if __name__ == "__main__":

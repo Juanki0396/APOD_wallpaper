f5cc8f553d64bb7769bad708db1c4abcc3957fdc
Juanki0396 <juanki0396@gmail.com>
2022-06-27 12:53:01 +0200

    Package initializer implemented

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,12 @@
-from apod import APODRetriever
+from src.apod_web import ApodExplorer
+from src.apod_image import ApodImageDownloader, Image
+
+
+def get_apod_image(year: int, month: int, day: int) -> Image:
+    """Return the APOD image for the selected date."""
+    explorer = ApodExplorer()
+    explorer.make_http_request(year, month, day)
+    img_url = explorer.check_for_images()
+    downloader = ApodImageDownloader(img_url)
+
+    return downloader.get_image()
